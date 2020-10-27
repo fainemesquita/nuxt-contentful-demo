c369dc08e72156a2b5e4bbb0d7261e6d7adc38d3
faine.mesquita <faine.mesquita@gmail.com>
2020-10-27 20:03:15 +1100

    updated credentials

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -4,6 +4,6 @@ const contentful = require('contentful')
 if (process.env.CTF_SPACE_ID) {
     module.exports = contentful.createClient({
         space: process.env.CTF_SPACE_ID,
-        accessToken: process.env.CTF_CD_ACCESS_TOKEN
+        accessToken: process.env.CTF_CDA_ACCESS_TOKEN
     })
 }
